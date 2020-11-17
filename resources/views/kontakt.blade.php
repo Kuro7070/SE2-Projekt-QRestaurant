f27801e60857347dabbf943aa24500f636bf5b45
acq604 <Debora.Anane@haw-hamburg.de>
2020-11-17 18:43:35 +0100

    rough layout of contact page

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -1,37 +1,37 @@
 <div id="kontakt" class="col-sm-12 col-md-9 col-lg-6">
 
-    <div class="card">
-        <div class="card-body bg-danger">
-            <h2 class="card-title">Kontakt</h2>
-            {{--                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>--}}
-            <p class="card-text">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-                dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-                sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-                ipsum dolor sit amet.
+    <div class="card bg-warm-grey border-0"> {{--after changing backgrounf --> card transparent--}}
+        <div class="card-body">
+            <h1 class="card-title font-weight-bold">Kontakt</h1>
 
-                @for ($i = 0; $i < 5; $i++)
-                    <br>{{$i}}. Hallo Welt!
-                @endfor
-            </p>
+            <form>
+            <div class="container">
+                <div class="row justify-content-start">
+                    <div class="col-4">
+                        <div class="row">
+                            <div class="form-group text-white">
+                                <label for="formGroupExampleInput">Name</label>
+                                <input type="text" class="form-control rounder-borders" id="formGroupExampleInput" placeholder="">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group text-white">
+                                <label for="exampleInputEmail1">E-Mail</label>
+                                <input type="email" class="form-control rounder-borders" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                            <div class="form-group text-white">
+                                <label for="exampleFormControlTextarea1">Nachricht</label>
+                                <textarea class="form-control rounder-borders" id="exampleFormControlTextarea1" rows="8"></textarea>
+                                <button type="submit" class="btn btn-secondary mt-3">Submit</button>
+                            </div>
+                    </div>
+                </div>
+                </div>
+            </form>
 
-            <div class="row">
-                <div class="col-6">
-                    <a href="{{ url('login') }}">
-                        <button type="button" style="width: 100%" class="btn btn-outline-success">Login</button>
-                    </a>
-                    <p class="text-muted">Bereits registriert?</p>
-                </div>
-                <div class="col-6">
-                    <a href="{{ url('register') }}">
-                        <button type="button" style="width: 100%" class="btn btn-primary">Registrieren</button>
-                    </a>
-                    <p class="text-muted">Lorem ipsum dolor sit amet?</p>
-                </div>
-            </div>
 
         </div>
     </div>
