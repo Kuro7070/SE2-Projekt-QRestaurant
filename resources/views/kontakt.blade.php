0851525baf92dbfbfa13f6e84ab064d49b94dc54
acq604 <Debora.Anane@haw-hamburg.de>
2020-11-18 21:48:02 +0100

    first layout of contact page done

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -1,31 +1,33 @@
 <div id="kontakt" class="col-sm-12 col-md-9 col-lg-6">
 
-    <div class="card bg-warm-grey border-0"> {{--after changing backgrounf --> card transparent--}}
-        <div class="card-body">
-            <h1 class="card-title font-weight-bold">Kontakt</h1>
+    <div class="card bg-warm-grey border-0 mt-5"> {{--after changing backgrounf --> card transparent--}}
+        <div class="card-body mt-3 ml-4">
+            <h1 class="card-title font-weight-bold display-4 mb-3">Kontakt</h1>
 
             <form>
             <div class="container">
                 <div class="row justify-content-start">
-                    <div class="col-4">
+                    <div class="col-sm-6 pr-0">
                         <div class="row">
-                            <div class="form-group text-white">
+                            <div class="form-group text-white w-75">
                                 <label for="formGroupExampleInput">Name</label>
                                 <input type="text" class="form-control rounder-borders" id="formGroupExampleInput" placeholder="">
                             </div>
                         </div>
                         <div class="row">
-                            <div class="form-group text-white">
+                            <div class="form-group text-white w-75">
                                 <label for="exampleInputEmail1">E-Mail</label>
                                 <input type="email" class="form-control rounder-borders" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
                             </div>
                         </div>
                     </div>
-                    <div class="col-6">
+                    <div class="col-sm-6 pl-0">
                             <div class="form-group text-white">
                                 <label for="exampleFormControlTextarea1">Nachricht</label>
-                                <textarea class="form-control rounder-borders" id="exampleFormControlTextarea1" rows="8"></textarea>
-                                <button type="submit" class="btn btn-secondary mt-3">Submit</button>
+                                <textarea class="form-control rounder-borders" id="exampleFormControlTextarea1" rows="5"></textarea>
+                                <div class="row justify-content-center">
+                                        <button type="submit" class="btn border-0 bg-button-color mt-3">Nachricht senden</button>
+                                </div>
                             </div>
                     </div>
                 </div>
