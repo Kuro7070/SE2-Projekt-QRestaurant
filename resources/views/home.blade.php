5fbb6a3a386bc79dce4aae2d204cf396307240de
acq604 <Debora.Anane@haw-hamburg.de>
2021-01-20 17:51:41 +0100

    tutorial images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,31 +6,25 @@
     <div class="row h-50">
         <div class="col-4 p-5">
             <div class="card h-100 w-100 position-relative" style="border-radius: 20px">
-                <div class="h-75">
-                </div>
-                <div class="h-25 text-center text-black-50 img-tutorial" style="font-size: 32px ">
-                    <img src = "{{'../img/tutorial-anmelden.png'}}"  alt="Tutorial: Anmelden"/>
-                    Anmelden
+                <div class="h-25 text-center text-black-50" style="font-size: 32px ">
+                    <img class="img-tutorial" src = "{{ asset('/images/tutorial-anmelden.png') }}"  alt="Tutorial: Anmelden"/>
+{{--                    Anmelden--}}
                 </div>
             </div>
         </div>
         <div class="col-4 p-5">
             <div class="card h-100 w-100 position-relative" style="border-radius: 20px">
-                <div class="h-75">
-                </div>
                 <div class="h-25 text-center text-black-50" style="font-size: 32px ">
-                    <img src = "{{ asset('/images/tutorial-menu-hochladen.png') }}"  alt="Tutorial: Menu hochladen"/>
-                    Menu hochladen
+                    <img class="img-tutorial" src = "{{ asset('/images/tutorial-menu-hochladen.png') }}"  alt="Tutorial: Menu hochladen"/>
+{{--                    Menu hochladen--}}
                 </div>
             </div>
         </div>
         <div class="col-4 p-5">
             <div class="card h-100 w-100 position-relative" style="border-radius: 20px">
-                <div class="h-75">
-                </div>
                 <div class="h-25 text-center text-black-50" style="font-size: 32px ">
-                    <img src = "../img/tutotial-fertiger-qr-code.png"  alt="Tutorial: Fertiger QR-Code"/>
-                    QR Code generieren
+                    <img class="img-tutorial" src = "{{ asset('/images/tutotial-fertiger-qr-code.png') }}" alt="Tutorial: Fertiger QR-Code"/>
+{{--                    QR Code generieren--}}
                 </div>
             </div>
         </div>
